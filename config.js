281b70fc14f3b41247e2a9853203dad6315a8be3
atroberg <alexistroberg@gmail.com>
2014-03-17 12:01:34 +0100

    Sample config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    productionDb: 'mongodb://localhost/apps4europe',
+    testDb: 'mongodb://localhost/test',
+  },
+  test: {
+    restURI: 'http://localhost:3001',
+  },
+};
